97b87ab843e389f62c9a68a28bce54a08ec7f598
José Roberto de Sousa Brito Júnior <81085769+Josejr3103@users.noreply.github.com>
2026-04-26 17:06:01 -0300

    Supabase update
    Criei os arquivos que ajudarão a utilizar o supabase no projeto, além de ajuste de nomes conflitantes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'services/supabase_service.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  await SupabaseService().initialize();
   runApp(const GameVaultApp());
 }
 
